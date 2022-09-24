ae197f1d1d34cd790d1a2daf278e4e6e556a941e
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-23 21:05:43 -0700

    initial commit for node backend

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 3000;
+
+
+const server = http.createServer(function(req, res){
+
+})
+
+server.listen(port, function(error) {
+  if (error) {
+    console.log("something went wrong", error)
+  } else {
+    console.log("sever is running on port " + port)
+  }
+})
